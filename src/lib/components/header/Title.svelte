31672cda19ee711967f5f6ddb6adf640a6ca0359
Rebecca Parks <git@rlparks.net>
2025-12-03 08:49:17 -0500

    Keep title text white even in light mode

--- src/lib/components/header/Title.svelte
+++ src/lib/components/header/Title.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <a class="h-full w-full bg-bulldog p-4" href={resolve("/")}>
-	<h1 class="mb-1 font-[Oswald] text-2xl font-bold">Carboy</h1>
+	<h1 class="mb-1 font-[Oswald] text-2xl font-bold text-white">Carboy</h1>
 </a>
