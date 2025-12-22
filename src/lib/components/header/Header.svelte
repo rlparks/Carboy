aebe9ec3034aaa4b79e6642fb4d24cbed19b3897
Rebecca Parks <git@rlparks.net>
2025-12-21 23:42:39 -0500

    Add mobile nav

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { onNavigate } from "$app/navigation";
 	import Button from "$lib/components/Button.svelte";
 	import { logout } from "$lib/components/header/account.remote";
 	import NavLink from "$lib/components/header/NavLink.svelte";
@@ -6,6 +7,7 @@
 	import Title from "$lib/components/header/Title.svelte";
 	import type { FriendlyAccount } from "$lib/types/bonus";
 	import type { Organization } from "$lib/types/db";
+	import { slide } from "svelte/transition";
 
 	type Props = {
 		account: FriendlyAccount | null;
@@ -14,50 +16,97 @@
 	};
 
 	let { account, organizations, selectedOrganizationId }: Props = $props();
+
+	let mobileNavOpen = $state(false);
+
+	onNavigate(() => {
+		mobileNavOpen = false;
+	});
 </script>
 
-<header class="mb-2 flex justify-between border-b-4 border-bulldog">
-	<nav class="flex items-center space-x-4">
-		<Title />
-		<ul class="flex items-center space-x-4 py-4">
-			{#if account}
-				<li>
-					<NavLink href="/" text="Home" mode="exact" />
-				</li>
-				<li>
-					<NavLink href="/trips" text="Trips" mode="exact" />
-				</li>
-				<!-- TODO: dashboard -->
-			{/if}
+<header class="mb-2 border-b-4 border-bulldog">
+	<div class="flex items-center justify-between">
+		<nav class="flex items-center space-x-4">
+			<Title />
+			<ul class="hidden items-center space-x-4 py-4 md:flex">
+				{@render navItems()}
+			</ul>
+		</nav>
 
-			{#if account?.role === "admin" || account?.role === "superadmin"}
-				<li>
-					<NavLink href="/admin/vehicles" comparePath="/admin" text="Admin" mode="startsWith" />
-				</li>
-			{/if}
+		<div class="hidden items-center space-x-4 p-4 md:flex">
+			{@render accountManagement()}
+		</div>
 
-			{#if account?.role === "superadmin"}
-				<li>
-					<NavLink
-						href="/super/organizations"
-						comparePath="/super"
-						text="Superadmin"
-						mode="startsWith"
-					/>
-				</li>
-			{/if}
-		</ul>
-	</nav>
-
-	<div class="flex items-center space-x-4 p-4">
-		{#if !account}
-			<Button href="/login/oidc">Login</Button>
-		{:else}
-			<OrganizationSwitcher {organizations} {selectedOrganizationId} />
-			<p>{account.name}</p>
-			<form {...logout}>
-				<Button type="submit">Logout</Button>
-			</form>
-		{/if}
+		<button
+			class="cursor-pointer md:hidden"
+			title="Toggle mobile navigation"
+			onclick={() => (mobileNavOpen = !mobileNavOpen)}
+		>
+			<!-- https://fonts.google.com/icons -->
+			<!-- menu -->
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" class="mr-4 w-10 text-white">
+				<path
+					fill="currentColor"
+					d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"
+				/>
+			</svg>
+		</button>
 	</div>
+
+	{#if mobileNavOpen}
+		<!-- hidden in case someone is having fun resizing their browser -->
+		<!-- or maybe rotates a mobile device -->
+		<nav transition:slide class="space-y-2 p-4 md:hidden">
+			<ul class="space-y-2">
+				{@render navItems()}
+			</ul>
+
+			<div class="flex w-full justify-end">
+				<div class="flex items-center space-x-4 text-xs">
+					{@render accountManagement()}
+				</div>
+			</div>
+		</nav>
+	{/if}
 </header>
+
+{#snippet navItems()}
+	{#if account}
+		<li>
+			<NavLink href="/" text="Home" mode="exact" />
+		</li>
+		<li>
+			<NavLink href="/trips" text="Trips" mode="exact" />
+		</li>
+		<!-- TODO: dashboard -->
+	{/if}
+
+	{#if account?.role === "admin" || account?.role === "superadmin"}
+		<li>
+			<NavLink href="/admin/vehicles" comparePath="/admin" text="Admin" mode="startsWith" />
+		</li>
+	{/if}
+
+	{#if account?.role === "superadmin"}
+		<li>
+			<NavLink
+				href="/super/organizations"
+				comparePath="/super"
+				text="Superadmin"
+				mode="startsWith"
+			/>
+		</li>
+	{/if}
+{/snippet}
+
+{#snippet accountManagement()}
+	{#if !account}
+		<Button href="/login/oidc">Login</Button>
+	{:else}
+		<OrganizationSwitcher {organizations} {selectedOrganizationId} />
+		<p>{account.name}</p>
+		<form {...logout}>
+			<Button type="submit">Logout</Button>
+		</form>
+	{/if}
+{/snippet}
