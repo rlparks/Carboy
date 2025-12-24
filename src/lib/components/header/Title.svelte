c79de92309aa52e2303ef3694a49e481b49807d5
Rebecca Parks <git@rlparks.net>
2025-12-24 07:27:42 -0500

    Adjust title left pad

--- src/lib/components/header/Title.svelte
+++ src/lib/components/header/Title.svelte
@@ -3,7 +3,7 @@
 	import logo from "$lib/assets/carboy-icon.png";
 </script>
 
-<a class="flex h-full w-full items-center gap-1 bg-bulldog p-4" href={resolve("/")}>
+<a class="flex h-full w-full items-center gap-1 bg-bulldog p-4 pl-2.5" href={resolve("/")}>
 	<img src={logo} alt="A small icon of a car" class="w-10" />
 	<h1 class="mb-1 font-[Oswald] text-3xl font-bold text-white">Carboy</h1>
 </a>
